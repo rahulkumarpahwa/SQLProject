9153e2101d53510111fc7f321f29bb0ba7a46c32
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2024-10-24 20:20:11 +0530

    created changes in the intial database and also created and added  csv results

--- Agrregate_Functions/OrderBy_Function.sql
+++ Agrregate_Functions/OrderBy_Function.sql
@@ -1,4 +1,4 @@
--- Description : This part of the query retrieves all columns (*) from the Customers table sorted in ascending order (ASC) based on the LastName column.
-
+-- Description : This part of the query retrieves all columns (*) from the Customers table sorted in ascending order (ASC) based on 
+the LastName column.
 SELECT * FROM Customers
 ORDER BY LastName ASC;
